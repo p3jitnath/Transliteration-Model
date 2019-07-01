911049c927d424a414b7594c102086709ad9a1e0
Pritthijit Nath <nathzi1505@Pritthijits-MacBook-Pro.local>
2019-07-01 17:24:12 +0530

    Developed the API

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # RNN Xelpmoc Intern
+We have developed a transliteration model which transliterates বাংলা to English, with an accuracy of approximately 96.5 %.
+
+## Important Files Directory
+- `transliteration_model.ipynb` - This the Jupyter notebook file which was used to train the transliteration model. The neural network was trained on a Google Cloud Platform instance having 2vCPUs and a single NVIDIA Tesla T4 GPU, taking around 5 minutes for a single epoch.   
+
+- `transliteration_model_usage.ipynb` - This the Jupyter notebook file which was used to develop the API `use_model.py`. It includes all the necessary preprocessing required to feed the বাংলা word to the API, so as to get the necessary output.
+
+- `transliteration_model.h5` - This is the trained Keras Sequential Model which does all the prediction for us.  
+
+- `use_model.py` - This is the API which we have developed, that takes the input through a command line argument, and gives the output in the file `output.txt`.   
+
+- `data/` - This folder contains the dataset on which the neural network was trained.
+
+## Additional Feature
+- `machine_translation.ipynb` - This Jupyter Notebook contains 5 models which developed a translation model which could convert English to French. The hybrid model is the one on which we based our final transliteration model. [Credit : Tommy Tracey]
+
+## Note
+- Version of TensorFlow used : `1.11.0`
+- Version of Keras used : `2.2.4`
+
+*** Please do not remove any file while tampering with this repo as removal may cause unwanted errors, which may be difficult to debug ***
+
+### Usage :
+Correct Usage of API : `python3 use_model.py -word <bengali_word>`
--- use_model.py
+++ use_model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 from keras import backend as K
 def custom_sparse_categorical_accuracy(y_true, y_pred):
@@ -7,48 +8,21 @@ def custom_sparse_categorical_accuracy(y_true, y_pred):
                   K.floatx())
 
 def input_preprocess(word, tokenizer, sequence_length):
-    """
-    Preprocess the input
-    Return : Preprocessed input and the Tokenizer
-    """
     preprocess_x = tokenizer.texts_to_sequences(word)
-
     result = [0 for i in range(sequence_length)]
-
     k = 0
-
     for i in range(len(preprocess_x)):
         try :
             result[k] = preprocess_x[i][0]
             k = k + 1
         except :
             pass
-
     return np.array([result]).astype('int32')
 
 def logits_to_text(logits, tokenizer):
-    """
-    Turn logits from a neural network into text using the tokenizer
-    :param logits: Logits from a neural network
-    :param tokenizer: Keras Tokenizer fit on the labels
-    :return: String that represents the text of the logits
-    """
     index_to_words = {id: word for word, id in tokenizer.word_index.items()}
     index_to_words[0] = ' '
-
     return ' '.join([index_to_words[prediction] for prediction in np.argmax(logits, 1)])
-
-def get_prediction(bengali_word, bengali_tokenizer, max_seq_length, english_tokenizer):
-    p_word = pre_processed_bengali_word(bengali_word)
-    preproc_input = input_preprocess(p_word, bengali_tokenizer, max_seq_length)
-    prediction = logits_to_text(model.predict(preproc_input)[0], english_tokenizer)
-
-    result = ""
-    for i in prediction.strip():
-        if i != ' ':
-            result = result + i.upper()
-
-    return result
 
 def pre_processed_bengali_word(bengali_word):
     result = ""
@@ -56,20 +30,47 @@ def pre_processed_bengali_word(bengali_word):
         result = result + i + " "
     return result
 
-######################################################################################
-# Loading of necessary files
+def load_files():
+    from keras.models import load_model
+    model = load_model('transliteration_model.h5', custom_objects={'custom_sparse_categorical_accuracy': custom_sparse_categorical_accuracy})
 
-from keras.models import load_model
-model = load_model('transliteration_model.h5', custom_objects={'custom_sparse_categorical_accuracy': custom_sparse_categorical_accuracy})
-import pickle
+    import pickle
+    with open("ben_token.pickle", "rb") as fp:
+        bengali_tokenizer = pickle.load(fp)
+    with open("eng_token.pickle", "rb") as fp:
+        english_tokenizer = pickle.load(fp)
+    with open("max_seq_length.txt", "rb") as fp:
+        max_bengali_seq_length = int(fp.read())
+    return model, bengali_tokenizer, english_tokenizer, max_bengali_seq_length
 
-with open("ben_token.pickle", "rb") as fp:
-    bengali_tokenizer = pickle.load(fp)
+def get_prediction(model, bengali_word, bengali_tokenizer, max_seq_length, english_tokenizer):
+    p_word = pre_processed_bengali_word(bengali_word)
+    preproc_input = input_preprocess(p_word, bengali_tokenizer, max_seq_length)
+    prediction = logits_to_text(model.predict(preproc_input)[0], english_tokenizer)
+    result = ""
+    for i in prediction.strip():
+        if i != ' ':
+            result = result + i.upper()
+    return result
 
-with open("eng_token.pickle", "rb") as fp:
-    english_tokenizer = pickle.load(fp)
 
-with open("max_seq_length.txt", "rb") as fp:
-    max_bengali_seq_length = int(fp.read())
+def main():
+    usage = "Correct Usage : python3 use_model.py -word <bengali_word>"
+    bengali_word = None
+    for i, word in enumerate(sys.argv):
+        if word == "-word":
+            try :
+                bengali_word = sys.argv[i + 1]
+            except :
+                bengali_word == None
 
-######################################################################################
+    if bengali_word == None:
+        print(usage)
+        return -1
+    model, bengali_tokenizer, english_tokenizer, max_bengali_seq_length = load_files()
+    result = get_prediction(model, bengali_word, bengali_tokenizer, max_bengali_seq_length, english_tokenizer)
+    with open("output.txt", "w+") as fp:
+        fp.write(result)
+
+if __name__ == "__main__":
+    main()
